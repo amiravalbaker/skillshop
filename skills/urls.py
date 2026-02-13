b4ad81b74f0cb74636e9333f8c8c5f2877e4275b
amiravalbaker <amiravb@yahoo.co.uk>
2026-02-13 17:53:46 +0000

    create profile page

--- skills/urls.py
+++ skills/urls.py
@@ -3,5 +3,6 @@ from . import views # import views.py from the current directory
 
 urlpatterns = [ 
     path('', views.home, name='home'), 
-    path("signup/", views.signup, name="signup"),
+    path("profile/", views.profile_view, name="profile"),
+    path("profile/edit/", views.edit_profile, name="edit_profile"),  
 ]
--- skills/views.py
+++ skills/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse 
-from .forms import SignUpForm
+from .forms import ProfileForm, SignUpForm
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request): return HttpResponse("Hello, World!")
+
+def home(request):
+    return render(request, "skills/home.html")
 
 def signup(request):
     if request.method == "POST":
@@ -18,6 +21,24 @@ def signup(request):
     return render(request, "skills/signup.html", {"form": form})
 
 @login_required
-def home(request):
-    return render(request, "skills/home.html")
+def profile_view(request):
+    profile = request.user.profile
+    return render(request, "skills/profile.html", {
+        "profile": request.user.profile
+    })
 
+@login_required
+def edit_profile(request):
+    profile = request.user.profile
+
+    if request.method == "POST":
+        form = ProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect("profile")
+    else:
+        form = ProfileForm(instance=profile)
+
+    return render(request, "skills/edit_profile.html", {
+        "form": form
+    })
